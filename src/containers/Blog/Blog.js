d1c6347355001e92d10e223eef17bd2dcfc83d51
Will <will.edwards@me.com>
2018-06-01 17:13:58 +0100

    ch154

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-import axios from 'axios';
 
 class Blog extends Component {
-
-    state = { 
+    state = {
         posts: [],
-        selectedPostId:'' 
+        selectedPostId: null
     }
 
     componentDidMount () {
-         axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                const posts = response.data.slice(0,4); //take the first 4 only
+        axios.get( 'https://jsonplaceholder.typicode.com/posts' )
+            .then( response => {
+                const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
                     return {
                         ...post,
                         author: 'Max'
                     }
-                })
-                this.setState({posts:updatedPosts});
-            });
+                });
+                this.setState({posts: updatedPosts});
+                // console.log( response );
+            } );
     }
 
     postSelectedHandler = (id) => {
@@ -32,13 +32,12 @@ class Blog extends Component {
     }
 
     render () {
-
         const posts = this.state.posts.map(post => {
             return <Post 
-                    clicked={() => this.postSelectedHandler(post.id)}
-                    title={post.title} 
-                    key={post.id} 
-                    author={post.author} />
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />;
         });
 
         return (
